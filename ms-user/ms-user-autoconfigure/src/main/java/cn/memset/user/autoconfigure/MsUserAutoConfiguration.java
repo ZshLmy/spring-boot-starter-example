fc70bd066a5634c714b9bcc1e4e9513072986450
zshlmy <zshlmy@163.com>
2023-08-27 21:50:06 +0800

    feign解释

--- ms-user/ms-user-autoconfigure/src/main/java/cn/memset/user/autoconfigure/MsUserAutoConfiguration.java
+++ ms-user/ms-user-autoconfigure/src/main/java/cn/memset/user/autoconfigure/MsUserAutoConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Import;
 
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnClass(UserFeignMarker.class)
-@EnableFeignClients(basePackageClasses = UserFeignMarker.class)
+@EnableFeignClients(basePackageClasses = UserFeignMarker.class) //UserFeignMarker这个类定义的目的就是标识出 basePackage 是什么，避免写具体的basePackage
 @EnableConfigurationProperties(MsUserConfiguration.class)
 @Import({UserFeignClient.UserFallbackFactory.class})
 public class MsUserAutoConfiguration {
